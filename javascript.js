ec83d449921b13ecce97d0dbe642e612279dddfd
UgandaWeapon-dev <jasonh17701coding@gmail.com>
2024-12-25 16:30:14 -0800

    Create 'index.html' and 'javascript.js'

---
+++ javascript.js
@@ -0,0 +1,26 @@
+/*
+
+playGame
+-Make playRound function and score variable inside playGame function
+- While playRound >= 5, continue game, else end game
+
+Get User's input
+- create function 'getHumanChoice'
+- prompt to get user's input
+    - validate input for choice
+
+Generate Computer's input
+- create function 'getComputerChoice'
+    - this function will randomly return string values: "rock", "paper", and "scissors"
+
+Track score
+- Create variables 'humanScore' and 'computerScore' in global scope
+    - Initialize variable with value: 0
+
+Determine playRound
+- Define two parameters: 'humanChoice' and 'computerChoice'
+    - make human choices case-insensitive
+- Write string value declaring the round winner
+- Increment humanScore / computerScore based on winner
+
+*/
